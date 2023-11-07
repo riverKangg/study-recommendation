0eb8afd0aaa19e342e07d4a6cab5e0f8d5307fa4
riverKangg <riverKangg@gmail.com>
2023-11-08 07:32:23 +0900

    Added files

---
+++ Neural_CF/get_data.py
@@ -0,0 +1,157 @@
+import pandas as pd
+import numpy as np
+
+rating = pd.read_csv('input/tmdb-movie-metadata/ratings_small.csv')
+df = rating[['userId', 'movieId', 'rating']]
+df.columns = ['user','item','plays']
+
+df.to_csv('data_neural_cf.csv',index=False)
+print('user 수:', len(np.unique(list(df['user']))))
+print('artist 수:', len(np.unique(list(df['item']))))
+
+
+def prepare_analy_dataset(df):
+    """
+    데이터 로드 함수
+
+    uids: train user
+    iids: train item
+    users: 전체 user
+    items: 전체 item
+    df_train: train data
+    df_test: test data
+    """
+    # user 10000명 샘플링
+    unique_user_lst = list(np.unique(df['user']))  # 358857명
+    sample_user_idx = np.random.choice(len(unique_user_lst), 671, replace=False)
+    sample_user_lst = [unique_user_lst[idx] for idx in sample_user_idx]
+
+    df = df[df['user'].isin(sample_user_lst)]
+    df = df.reset_index(drop=True)
+
+    # 1명 이상의 artist데이터가 있는 user만 사용
+    df_count = df.groupby(['user']).count()
+    df['count'] = df.groupby('user')['user'].transform('count')
+    df = df[df['count'] > 1]
+
+    # user, item 아이디 부여
+    df['user_id'] = df['user'].astype("category").cat.codes
+    df['item_id'] = df['item'].astype("category").cat.codes
+
+    # lookup 테이블 생성
+    item_lookup = df[['item_id', 'item']].drop_duplicates()
+    item_lookup['item_id'] = item_lookup.item_id.astype(str)
+
+    # train, test 데이터 생성
+    df = df[['user_id', 'item_id', 'plays']]
+    df_train, df_test = train_test_split(df)
+
+    # 전체 user, item 리스트 생성
+    users = list(np.sort(df.user_id.unique()))
+    items = list(np.sort(df.item_id.unique()))
+
+    # train user, item 리스트 생성
+    rows = df_train['user_id'].astype(int)
+    cols = df_train['item_id'].astype(int)
+    values = list(df_train.plays)
+
+    uids = np.array(rows.tolist())
+    iids = np.array(cols.tolist())
+
+    # 각 user마다 negative item 생성
+    df_neg = get_negatives(uids, iids, items, df_test)
+
+    return uids, iids, df_train, df_test, df_neg, users, items, item_lookup
+
+
+def get_negatives(uids, iids, items, df_test):
+    """
+    negative item 리스트 생성함수
+    """
+    negativeList = []
+    test_u = df_test['user_id'].values.tolist()
+    test_i = df_test['item_id'].values.tolist()
+
+    test_ratings = list(zip(test_u, test_i))  # test (user, item)세트
+    zipped = set(zip(uids, iids))  # train (user, item)세트
+
+    for (u, i) in test_ratings:
+
+        negatives = []
+        negatives.append((u, i))
+        for t in range(100):
+            j = np.random.randint(len(items))  # neg_item j 1개 샘플링
+            while (u, j) in zipped:  # j가 train에 있으면 다시뽑고, 없으면 선택
+                j = np.random.randint(len(items))
+            negatives.append(j)
+        negativeList.append(negatives)  # [(0,pos), neg, neg, ...]
+
+    df_neg = pd.DataFrame(negativeList)
+
+    return df_neg
+
+
+def mask_first(x):
+    result = np.ones_like(x)
+    result[0] = 0  # [0,1,1,....]
+
+    return result
+
+
+def train_test_split(df):
+    """
+    train, test 나누는 함수
+    """
+    df_test = df.copy(deep=True)
+    df_train = df.copy(deep=True)
+
+    # df_test
+    # user_id와 holdout_item_id(user가 플레이한 아이템 중 1개)뽑기
+    df_test = df_test.groupby(['user_id']).first()
+    df_test['user_id'] = df_test.index
+    df_test = df_test[['user_id', 'item_id', 'plays']]
+    df_test = df_test.reset_index(drop=True)
+
+    # df_train
+    # user_id 리스트에 make_first()적용
+    mask = df.groupby(['user_id'])['user_id'].transform(mask_first).astype(bool)
+    df_train = df.loc[mask]
+
+    return df_train, df_test
+
+
+def get_train_instances(uids, iids, num_neg, num_items):
+    """
+    모델에 사용할 train 데이터 생성 함수
+    """
+    user_input, item_input, labels = [], [], []
+    zipped = set(zip(uids, iids))  # train (user, item) 세트
+
+    for (u, i) in zip(uids, iids):
+
+        # pos item 추가
+        user_input.append(u)  # [u]
+        item_input.append(i)  # [pos_i]
+        labels.append(1)  # [1]
+
+        # neg item 추가
+        for t in range(num_neg):
+
+            j = np.random.randint(num_items)  # neg_item j num_neg(4)개 샘플링
+            while (u, j) in zipped:  # u가 j를 이미 선택했다면
+                j = np.random.randint(num_items)  # 다시 샘플링
+
+            user_input.append(u)  # [u1, u1,  u1,  ...]
+            item_input.append(j)  # [pos_i, neg_j1, neg_j2, ...]
+            labels.append(0)  # [1, 0,  0,  ...]
+
+    return user_input, item_input, labels
+
+uids, iids, df_train, df_test, df_neg, users, items, item_lookup = prepare_analy_dataset(df)
+print(df_train.shape)
+
+
+# train data
+user_input, item_input, labels = get_train_instances(uids, iids, num_neg=4, num_items=len(items))
+print('df_train의 첫번째 행: (user_id, item_id)=', (uids[0], iids[0]))
+print('df_train의 두번째 행: (user_id, item_id)=', (uids[1], iids[1]))
